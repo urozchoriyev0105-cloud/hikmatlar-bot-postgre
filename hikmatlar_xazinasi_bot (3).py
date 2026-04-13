ef34cb9bf7ec7a07e949ba6bb1b7d9c6ef98f73d
urozchoriyev0105-cloud <urozchoriyev0105@gmail.com>
2026-04-13 17:09:15 +0500

    Update hikmatlar_xazinasi_bot (3).py

--- hikmatlar_xazinasi_bot (3).py
+++ hikmatlar_xazinasi_bot (3).py
@@ -644,17 +644,20 @@ def broad_send(message):
         )
 
     except Exception:
-    bot.send_message(
-        message.chat.id,
-        "❌ Xatolik",
-        reply_markup=admin_keyboard()
-    )
+        bot.send_message(
+            message.chat.id,
+            "❌ Xatolik",
+            reply_markup=admin_keyboard()
+        )
 
     bot.send_message(
         message.chat.id,
         "Admin paneli:",
         reply_markup=admin_keyboard()
     )
+
+
+        
 
             
 @bot.message_handler(func=lambda m: m.text == "📚 Saqlangan hikmatlar")
